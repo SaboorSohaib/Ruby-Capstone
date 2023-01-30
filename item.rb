b5261bbea1e46cdb45fcafd73136c8baded16685
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-30 21:43:24 +0500

    add collaborative work with project

--- item.rb
+++ item.rb
@@ -1,8 +1,16 @@
 class Item
-    def initialize(publish_date, archived)
+    def initialize(publish_date, archived: false)
         @id = Random.rand(1..100)
         @publish_date = publish_date
         @archived = archived
     end
-    
+
+    def move_to_archive
+        can_be_archived? || @archived
+    end
+
+    private
+    def can_be_archived?
+        @publish_date > 10
+    end
 end
