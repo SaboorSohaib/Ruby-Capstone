be04f88f8f852396a342c0ef82c55ba7afb7c3c4
RashidMenhas <rashidmenhas10@gmail.com>
2023-02-01 02:20:51 +0500

    Add Accessor name, and items array.

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,5 +1,12 @@
 class Genre
-    def initialize(name)
-        
+    attr_accessor :name, :id
+    attr_reader :items
 
+    def initialize(name)
+        @id = Random.rand(1..1000)
+        @name = name
+        @items = []
+    end
+
+    
 end
