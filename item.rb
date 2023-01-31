12e249cdf79679c27dd096d0462ca5791112d676
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-31 13:23:47 +0500

    Create can_be_archived and move_to_archived methods

--- item.rb
+++ item.rb
@@ -1,16 +1,17 @@
 class Item
-    def initialize(publish_date, archived: false)
-        @id = Random.rand(1..100)
-        @publish_date = publish_date
-        @archived = archived
-    end
+  def initialize(publish_date, _archived)
+    @id = Random.rand(1..100)
+    @publish_date = publish_date
+    @archived = false
+  end
 
-    def move_to_archive
-        can_be_archived? || @archived
-    end
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 
-    private
-    def can_be_archived?
-        @publish_date > 10
-    end
+  private
+
+  def can_be_archived?
+    (Time.new.year - @publish_date.year) > 10
+  end
 end
