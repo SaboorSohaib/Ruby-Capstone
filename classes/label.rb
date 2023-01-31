c5188a768c5e9b91466f7611c99d05251c909c22
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-01-31 20:23:39 +0430

    fix linter errors

--- classes/label.rb
+++ classes/label.rb
@@ -1,6 +1,6 @@
 class Lable
-    attr_accessor :title :color
-    attr_reader :itme
+  attr_accessor :title, :color
+  attr_reader :itme
 
   def initialize(_id, title, color, _itmes)
     @id = Random.rand(1..1000)
