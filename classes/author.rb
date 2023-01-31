b933ca42ef2588b8e98b49f8a2507da02485dd72
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-31 20:07:32 +0100

    ADD attr_accessor AND attr_reader TO Author Class

--- classes/author.rb
+++ classes/author.rb
@@ -1,4 +1,7 @@
 class Author
+  attr_accessor :first_name, :last_name, :id
+  attr_reader :items
+
   def initialize(first_name, last_name)
     @id = Random.rand(1...100)
     @first_name = first_name
