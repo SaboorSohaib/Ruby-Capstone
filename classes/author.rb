beba90fa5836fbb833a5ab42bb4f8d3545cfa5f9
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-31 20:06:59 +0100

    ADD initialize METHOD TO Author CLASS

--- classes/author.rb
+++ classes/author.rb
@@ -1,3 +1,8 @@
 class Author
-  puts 'Author'
+  def initialize(first_name, last_name)
+    @id = Random.rand(1...100)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
 end
