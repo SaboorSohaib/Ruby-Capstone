eaa7505349195fe37c7f866f6730b10edd1230b3
RashidMenhas <rashidmenhas10@gmail.com>
2023-02-01 18:19:46 +0500

    Used correct format in the given constructor parametor

--- classes/musicalbum.rb
+++ classes/musicalbum.rb
@@ -3,7 +3,7 @@ require_relative '../item'
 class MusicAlbum < Item
   attr_accessor :on_spotify, :publish_date, :name
 
-  def initialize(on_spotify, publish_date, name)
+  def initialize(name, publish_date, on_spotify)
     super(publish_date)
     @id = Random.rand(1..1000)
     @on_spotify = on_spotify
