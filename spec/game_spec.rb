fecf7545b02fab2aec2207bd604bfbfce9b20420
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-01 12:52:13 +0100

    Add test for can_be_archived method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,4 +6,16 @@ describe Game do
     @game1 = Game.new Time.local(2022), 'should check the test_multiplayer', Time.local(2018)
     @game2 = Game.new Time.new(2009), Time.new(2018), true
   end
+
+  describe '#NewGame' do
+    it 'should check the instance of test game if it is an instance of the Game class' do
+      expect(@game1).to be_instance_of Game
+    end
+  end
+
+  describe 'can_be_archived?' do
+    it 'Can not use can_be_archived method when it private' do
+      expect(@game).not_to respond_to(:can_be_archived?)
+    end
+  end 
 end
