bad6df6e43c328348d70ec6cca98243aaa50a521
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-01 14:28:57 +0100

    ADD TEST TO CHECK THE INSTANCE OF NEW Author OBJECT

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -6,4 +6,8 @@ describe Author do
   before :each do
     @author = Author.new 'test_first_name', 'test_last_name'
   end
+
+  it 'should check if the test author is an instance of Author class' do
+    expect(@author).to be_instance_of Author
+  end
 end
