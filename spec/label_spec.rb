89ff1c977d72638dedcd35cda6e8d9de347454e3
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-01 15:51:35 +0430

    Add test for lable class

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,2 +1,22 @@
 require_relative '../classes/label'
 require_relative 'spec_helper'
+
+describe Lable do
+  context 'Testing the Label class'
+  title = 'Javascript'
+  color = 'green'
+  label = Lable.new(title, color)
+
+  it 'should validate the title of the label' do
+    expect(label.title).to eq title
+  end
+
+  it 'should validate the color of the lable' do
+    expect(label.color).to eq color
+  end
+
+  it 'should add item peoperly' do
+    label.add_item(item)
+    expect(label.items.length).to eq 1
+  end
+end
