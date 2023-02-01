5ed860fc80727fa5a7d3618e2cb68ef0f73f55af
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-01 12:48:24 +0100

    Create new instance of Game Object

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,5 +2,8 @@ require_relative '../classes/game'
 require_relative 'spec_helper'
 
 describe Game do
-
+  before :each do
+    @game1 = Game.new Time.local(2022), 'should check the test_multiplayer', Time.local(2018)
+    @game2 = Game.new Time.new(2009), Time.new(2018), true
+  end
 end
