058ab9cb82b234c44f67c31c1fe9ee5a8ac33cef
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-01 14:29:47 +0100

    ADD TEST TO CHECK IF AUTHOR'S ITEMS LIST IS BEEN UPDATED

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -10,4 +10,10 @@ describe Author do
   it 'should check if the test author is an instance of Author class' do
     expect(@author).to be_instance_of Author
   end
+
+  it 'check if item has been added to the author items list' do
+    item = Item.new '2023/01/02'
+    @author.add_item(item)
+    expect(@author.items).to include(item)
+  end
 end
