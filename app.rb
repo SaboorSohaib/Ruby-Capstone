1b9d7ae11d73662dd6136186da29312198dc4457
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-02 14:24:55 +0430

    Add options for user one

--- app.rb
+++ app.rb
@@ -1,3 +1,49 @@
+require_relative './classes/book'
+require_relative './classes/lable'
+require_relative './item'
+
 class App
-  puts 'Welcome to our catalog project.'
+  def initialize
+    @books = []
+    @lables = []
+  end
+
+  def add_book
+    puts 'Publisher:'
+    publish = gets.chomp
+    puts 'Cover State:'
+    cover = gets.chomp
+    puts 'publish Date (yyyy-dd-mm):'
+    date = gets.chomp
+    book = Book.new(publish, cover, date)
+    @books.push(book)
+    puts 'Would you like to add lable? (1)- Yes // (2)- No'
+    options = gets.chomp.to_i
+    return unless options == 1
+
+    puts 'Choose a title for lable:'
+    tit = gets.chomp
+    puts 'Choose a color for the lable:'
+    color = gets.chomp
+    lable = Lable.new(tit, color)
+    @lables.push(lable)
+  end
+
+  def list_all_books
+    if @books.empty?
+      puts "Sorry, We do not have any book\n\n"
+    else
+      @books.each do |bk|
+        puts "Publisher: #{bk.publisher} Cover: #{bk.cover_state} Publish Date: #{bk.publish_date}\n"
+      end
+    end
+  end
+
+  def list_all_lables
+    if @lables.empty?
+      puts "Sorry, We do not have any lables\n\n"
+    else
+      @lables.each { |lab| puts "Lable Title: #{lab.title} of color #{lab.color}\n" }
+    end
+  end
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,27 @@
-class Main
-  puts 'Welcome to our catalog project'
+#!/usr/bin/env ruby
+require_relative './app'
+
+def main
+  app = App.new
+  response = nil
+
+  puts "Welcome Catalog App!\n\n"
+  while response != '4'
+    puts 'Please choose an option by enter in a number from 1 to 7:'
+    puts '1 - List all books'
+    puts '2 - List all lables'
+    puts '3 - Add a book'
+    response = gets.chomp
+    case response
+    when '1'
+      app.list_all_books
+    when '2'
+      app.list_all_lables
+    when '3'
+      app.add_book
+    when '4'
+      puts 'Thank you for using this app!'
+    end
+  end
 end
+main
