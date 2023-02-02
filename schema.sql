63bb9f15116faad25ba970231da4054783a7eee5
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-02 21:33:33 +0430

    Add author table

--- schema.sql
+++ schema.sql
@@ -20,3 +20,10 @@ id        INT GENERATED ALWAYS AS IDENTITY,
 name      VARCHAR(50),
 PRIMARY KEY (id)
 );
+
+CREATE TABLE author(
+id       INT GENERATED ALWAYS AS IDENTITY,
+first_name  VARCHAR(50),
+last_name   VARCHAR(50),
+PRIMARY KEY (id)
+);
