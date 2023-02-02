59f54116a52af9f18dd63b43ba53f910792420ac
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-02 20:16:00 +0100

    ADD options for games and authors in the main.rb FIle

--- main.rb
+++ main.rb
@@ -6,11 +6,14 @@ def main
   response = nil
 
   puts "Welcome Catalog App!\n\n"
-  while response != '4'
+  while response != '7'
     puts "Please choose an option by enter in a number from 1 to 7:\n\n"
     puts '1 - List all books'
     puts '2 - List all lables'
     puts '3 - Add a book'
+    puts '4 - Add a game'
+    puts '5 - List all games'
+    puts '6 - List all authors'
     response = gets.chomp
     case response
     when '1'
@@ -20,6 +23,12 @@ def main
     when '3'
       app.add_book
     when '4'
+      app.add_game
+    when '5'
+      app.list_all_games
+    when '6'
+      app.list_all_authors
+    when '7'
       puts 'Thank you for using this app!'
     end
   end
