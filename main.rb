e3f458fa5f49ea2bb7e11e6ecdfce36d150a599e
RashidMenhas <rashidmenhas10@gmail.com>
2023-02-03 15:04:27 +0500

    Add 7, 8, 9 option in a file

--- main.rb
+++ main.rb
@@ -7,14 +7,18 @@ def main
   response = nil
 
   puts "Welcome Catalog App!\n\n"
-  while response != '7'
-    puts "Please choose an option by enter in a number from 1 to 7:\n\n"
+  while response != '9'
+    puts "Please choose an option by enter in a number from 1 to 9:\n\n"
     puts '1 - List all books'
     puts '2 - List all lables'
     puts '3 - Add a book'
     puts '4 - Add a game'
     puts '5 - List all games'
     puts '6 - List all authors'
+    puts '7 - List of all Genre'
+    puts '8 - Add music Album '
+    puts '9 - Exit App'
+
     response = gets.chomp
     case response
     when '1'
@@ -30,6 +34,10 @@ def main
     when '6'
       app.list_all_authors
     when '7'
+      app.list_all_genres
+    when '8'
+      app.add_music_album
+    when '9'
       puts 'Thank you for using this app!'
     end
   end
